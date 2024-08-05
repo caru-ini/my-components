f8f69567a3e758016ade9a050468cced8c4991eb
caru-ini <caru17122@gmail.com>
2024-08-05 16:16:41 +0900

    feat: add status card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import { StatusCard } from '@/components/collection/statusCard';
 import ToggleTheme from '@/components/collection/toggletheme';
 import { Showcase } from '@/components/showcase';
 import { buttonVariants } from '@/components/ui/button';
 import { getSnippet } from '@/lib/getCodeSnippet';
+import { IceCream } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className='container flex flex-col gap-3 p-10'>
+    <div className='container flex flex-col p-10'>
       <div className='flex justify-between border-b-2 border-border py-2'>
         <h1 className='flex items-center gap-x-2 text-2xl'>
           <a
@@ -13,7 +15,7 @@ export default function Home() {
             className={buttonVariants({ variant: 'ghost', className: 'py-1 !text-2xl' })}
             target='_blank'
           >
-            caru-ini
+            {/* caru-ini */}
           </a>
           /
           <a
@@ -28,11 +30,19 @@ export default function Home() {
           <ToggleTheme />
         </div>
       </div>
-      <div className='flex flex-wrap gap-3'>
+      <div className='mt-5 flex flex-wrap gap-3'>
         {/* frame and code */}
         <Showcase title='Example' />
         <Showcase title='Theme Toggle' code={getSnippet('toggletheme')}>
           <ToggleTheme />
+        </Showcase>
+        <Showcase title='Status Card' code={getSnippet('statusCard')}>
+          <StatusCard
+            icon={<IceCream size={30} />}
+            title='IceCream'
+            status='success'
+            description='available'
+          />
         </Showcase>
       </div>
     </div>
---
+++ src/components/collection/statusCard.tsx
@@ -0,0 +1,51 @@
+import { cn } from '@/lib/utils';
+
+type Status = 'success' | 'warning' | 'error';
+
+type StatusCardProps = {
+  icon: JSX.Element;
+  title: string;
+  status: Status;
+  description?: string;
+};
+
+export const StatusCard: React.FC<StatusCardProps> = ({ icon, title, status, description }) => {
+  return (
+    <div className='flex items-center rounded-md border border-border p-2'>
+      <div className='size-full rounded-sm border border-border p-2'>{icon}</div>
+      <div className='ml-2 flex flex-col justify-center'>
+        <h2>{title}</h2>
+        <div className='flex items-center justify-between gap-x-1'>
+          <Indicator status={status} />
+          <p className='text-sm text-muted-foreground'>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Indicator: React.FC<{ status: Status }> = ({ status }) => {
+  const innerStatusStyles = {
+    success: 'bg-green-600',
+    warning: 'bg-yellow-600',
+    error: 'bg-red-600',
+  };
+
+  const outerStatusStyles = {
+    success: 'bg-green-300',
+    warning: 'bg-yellow-300',
+    error: 'bg-red-300',
+  };
+  return (
+    <div>
+      <div
+        className={cn(
+          `flex size-3 rounded-full items-center justify-center`,
+          outerStatusStyles[status],
+        )}
+      >
+        <div className={cn(`size-2 rounded-full`, innerStatusStyles[status])} />
+      </div>
+    </div>
+  );
+};
